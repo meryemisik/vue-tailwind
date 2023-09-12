2cfc1986f437cf3dd5688f49179dd45ba76c72f4
meryem <mmeryemisik@gmail.com>
2023-09-12 23:07:04 +0300

    fixed albume page

--- src/views/Albums.vue
+++ src/views/Albums.vue
@@ -8,20 +8,25 @@
       />
       <p class="font-poppins">Go Home</p>
     </div>
-    <div class="flex flex-wrap max-h-screen overflow-y-auto thin-scrollbar">
-      <div v-for="i in 12" :key="i" class="p-4 flex-grow flex-shrink-0 flex-basis-1/3 min-w-100 sm:flex-basis-1/2 md:flex-basis-1/1" @click="this.$router.push('albums/detail')">
+    <div class="max-h-screen overflow-y-auto thin-scrollbar flex flex-wrap justify-evenly">
+      <div v-for="(i,index) in 12" :key="i" class="p-4 flex-initial sm:w-full md:w-[49%] xl:w-[23%] lg:w-[32%] " @click="this.$router.push('albums/detail')">
         <div
           class="bg-white border rounded-xl shadow p-4 hover:shadow-lg transition duration-300 ease-in-out cursor-pointer min-w-[100%]"
         >
           <div class="flex flex-col gap-4 flex-start w-full">
             <div class="flex flex-start">
             <div class="flex-grow-0 flex-shrink-0 w-1/2 p-0">
-              <img src="../asset/image/album1.png" class="object-contain w-full"/>
-              <img src="../asset/image/album4.png" class="object-contain w-full"/>
+              <img v-if="index % 2 == 0" src="../asset/image/album1.png" class="object-contain w-full"/>
+              <img v-else src="../asset/image/album5.png"/>
+              <img v-if="index % 3 == 0" src="../asset/image/album8.png" class="object-contain w-full"/>
+              <img v-else src="../asset/image/album6.png"/>
+             
             </div>
             <div class="flex-grow-0 flex-shrink-0 w-1/2 p-0">
-              <img src="../asset/image/album3.png" class="object-contain w-full"/>
-              <img src="../asset/image/album2.png" class="object-contain w-full"/>
+              <img v-if="index % 4 == 0" src="../asset/image/album2.png" class="object-contain w-full"/>
+              <img v-else src="../asset/image/album4.png"/>
+              <img v-if="index % 5 == 0" src="../asset/image/album7.png" class="object-contain w-full"/>
+              <img v-else src="../asset/image/album3.png"/>
             </div>
           </div>
           </div>
